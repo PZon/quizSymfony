8c54a27e9fe21dd021bad5deb855f332476860a5
Pzon <piotrzon@gmail.com>
2022-08-31 14:59:09 +0200

    chptr 16 done

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class CommentController extends AbstractController{
     /**
      * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
      */
-    public function commentVote($id, $direction){
+    public function commentVote($id, $direction, LoggerInterface $logger){
         if ($direction ==='up'){
+            $logger->info('voting UP');
             $currentVoteCount = rand(7,13);
         }else{
+            $logger->info('voting DOWN');
             $currentVoteCount = rand(1,6);
         }
         //return new JsonResponse(['votes'=>$currentVoteCount]); //działanie takie samo jak poniżej
