7330f22a84b8b9e46bb356298c8150aa959d0d41
Pzon <piotrzon@gmail.com>
2022-09-06 14:44:37 +0200

    chptr2.3 done cache

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class QuestionController extends AbstractController
 {
@@ -22,7 +23,7 @@ class QuestionController extends AbstractController
     /**
      * @Route("/quests/{anyWord}", name="questionShow")
      */
-    public function show($anyWord, MarkdownParserInterface $markdownParser ){
+    public function show($anyWord, MarkdownParserInterface $markdownParser, CacheInterface $cache){
 
         $answers = [
             'answer `txt` One',
@@ -34,7 +35,10 @@ class QuestionController extends AbstractController
         //dd($anyWord, $this);
 
         $questionTxt = "I've been turned into a cat, any thoughts on how to turn back? While I'm **adorable**, I don't really care for cat food.";
-        $parsedQuestionTxt = $markdownParser->transformMarkdown($questionTxt);
+        $parsedQuestionTxt = $cache->get('markdown_'.md5($questionTxt), function() use ($questionTxt,$markdownParser){
+            return $markdownParser->transformMarkdown($questionTxt);
+        });
+       // $parsedQuestionTxt = $markdownParser->transformMarkdown($questionTxt);
 
         return $this->render('question/show.html.twig',[
             'question'=>ucwords(str_replace('-',' ',$anyWord)),
